d0890918485460ce8e512c007ecbe550071fd876
buck5060 <buck5060@gmail.com>
2016-06-04 13:43:07 +0800

    fix trans mismatch problem

--- read_file.h
+++ read_file.h
@@ -73,39 +73,39 @@ int fileReading()
 	for(input_idx = 0; input_idx < input_num;)
 	{
 		getline(netlist,str_temp,'(');
-		cout << str_temp << endl;
+		//cout << str_temp << endl;
 		if(str_temp ==  "INPUT")
 		{
 			getline(netlist,str_temp,')');
 			temp = stoi (str_temp,&sz);
 			input[input_idx].id = temp; 
-			cout << input[input_idx].id << endl;
+			//cout << input[input_idx].id << endl;
 			input_idx++; 
 		}
 		getline(netlist,str_temp);
 	}
 	
-	cout << "input_idx:" << input_idx << " / " << "input_num:" << input_num << endl;
+	//cout << "input_idx:" << input_idx << " / " << "input_num:" << input_num << endl;
 
 
 	getline(netlist,str_temp);
 	for(output_idx = 0; output_idx < output_num;)
 	{
 		getline(netlist,str_temp,'(');
-		cout << str_temp << endl;
+		//cout << str_temp << endl;
 		if(str_temp ==  "OUTPUT")
 		{
 			getline(netlist,str_temp,')');
 			temp = stoi (str_temp,&sz);
 			output[output_idx].id = temp; 
-			cout << output[output_idx].id << endl;
+			//cout << output[output_idx].id << endl;
 			output_idx++;
 		
 		}
 		getline(netlist,str_temp);
 	}
 	
-	cout << "output_idx:" << output_idx << " / " << "output_num:" << output_num << endl;
+	//cout << "output_idx:" << output_idx << " / " << "output_num:" << output_num << endl;
 
 	getline(netlist,str_temp);
 
@@ -113,44 +113,44 @@ int fileReading()
 	while( getline(netlist,str_temp, ' ') )
 	{
 	
-		cout << "//-------------gate recognize start----------------" << endl;
+		//cout << "//-------------gate recognize start----------------" << endl;
 
-		cout << endl << "Found output : " << str_temp << endl; 
+		//cout << endl << "Found output : " << str_temp << endl; 
 		output_idx = stoi (str_temp,&sz);
 
 		getline(netlist,str_temp, ' ');
 		getline(netlist,str_temp, '(');
-		cout << "Found Gate type : " << str_temp << endl;
+		//cout << "Found Gate type : " << str_temp << endl;
 		gates[gate_i].type = gate_type(str_temp);
-		cout << "Gate type:" << gates[gate_i].type << endl;
+		//cout << "Gate type:" << gates[gate_i].type << endl;
 
 		getline(netlist,str_temp, ')');
 
 		if(str_temp[3] != ',')
 		{
-			cout << "Found input : " << str_temp << endl << endl;
+			//cout << "Found input : " << str_temp << endl << endl;
 			input_idx = stoi (str_temp,&sz);
 			input_idx2 = 0;
 		}
 		else
 		{
-			cout << "Found input : " << str_temp << endl << endl;
+			//cout << "Found input : " << str_temp << endl << endl;
 			input_idx = stoi (str_temp.substr (0,3),&sz);
 			input_idx2 = stoi (str_temp.substr (4,6),&sz);
 		}
 		
-		cout << "gate_i : " << gate_i  << endl << "gate id : " << gates[gate_i].type << endl  << "in1 id : " << input_idx << endl << "in2 id : " << input_idx2 << endl << endl;
+		//cout << "gate_i : " << gate_i  << endl << "gate id : " << gates[gate_i].type << endl  << "in1 id : " << input_idx << endl << "in2 id : " << input_idx2 << endl << endl;
 		
 		//--------------------- in1 -------------------------
 		for(int i = 0; i < input_num; i++) //check if input_idx is input
 		{
-			cout << ".";
+			//cout << ".";
 			if(input[i].id == input_idx)
 			{
 				gates[gate_i].in1 = &input[i];
 				input[i].to = &gates[gate_i]; 
-				cout << "input found" << endl;
-				cout << "input as in1 of this gate:" << gates[gate_i].in1 -> id << endl ;
+				//cout << "input found" << endl;
+				//cout << "input as in1 of this gate:" << gates[gate_i].in1 -> id << endl ;
 				input_idx = 0;
 				break;
 			}
@@ -158,35 +158,49 @@ int fileReading()
 		
 		if(input_idx != 0) //new, using middle line
 		{
-			cout << "tans_i : " << trans_i << " / input_idx : " << input_idx << endl;
+			//cout << "tans_i : " << trans_i << " / input_idx : " << input_idx << endl;
+
+			//test part
+			if(input_idx == 401)
+			{
+				//cout << trans[5].id;
+			}
+
 			for(int i = 0; i < trans_i; i++)
 			{	
-				cout << "." ;
-				if(trans[i].id == output_idx)
+				//cout << "." ;
+				if(trans[i].id == input_idx)
 				{
-					cout << "trans for in1 found" << endl;
+
+					//cout << "exist trans for in1 found" << endl;
 					gates[gate_i].in1 = &trans[i];
 					trans[i].to = &gates[gate_i];
+
+					gates[gate_i].in1 -> id = input_idx;
+
+					input_idx = 0;
 					break;
 				}
 			}
 				
-			if (gates[gate_i].out == 0)
+			if (input_idx!= 0) //middle line doesn't exist
 			{
+				//cout << "using new trans" << endl;
 				gates[gate_i].in1 = &trans[trans_i];
 				trans[trans_i].to = &gates[gate_i];
+				gates[gate_i].in1 -> id = input_idx;
+				//cout << "trans id: " << trans[trans_i].id << endl;
 				trans_i++;
+				input_idx = 0;
 			}
 
 
-			gates[gate_i].in1 -> id = input_idx;
 
-
-			cout << endl << ">>> middle line as input1 of this gate(on mem):" << trans[trans_i].to ->type << endl ;
-			cout << "input as in1 of this gate:" << gates[gate_i].in1 -> id << endl ;
+			////it will not show the exist line assignment //cout << endl << ">>> middle line as input1 of this gate(on mem):" << trans[trans_i].to ->type << endl ;
+			//cout << "input as in1 of this gate:" << gates[gate_i].in1 -> id << endl ;
 		}
 
-		cout << "in1 process complete //" << endl << endl;
+		//cout << "in1 process complete //" << endl << endl;
 
 		// ----------------------- in2 --------------------------------
 		if(input_idx2 != 0) //check if input_idx2 is a input 
@@ -197,8 +211,8 @@ int fileReading()
 				{
 					gates[gate_i].in2 = &input[i];
 					input[i].to = &gates[gate_i];
-					cout << "input found" << endl;
-					cout << "input as in2 of this gate:" << gates[gate_i].in2 -> id << endl ;
+					//cout << "input found" << endl;
+					//cout << "input as in2 of this gate:" << gates[gate_i].in2 -> id << endl ;
 					input_idx2 = 0;
 					break;
 				}
@@ -207,39 +221,42 @@ int fileReading()
 
 			if(input_idx2 != 0) //new, using middle line
 			{
-				temp = 0;
-				cout << "tans_i : " << trans_i << " / input_idx2 : " << input_idx2 << endl;
+				//cout << "tans_i : " << trans_i << " / input_idx2 : " << input_idx2 << endl;
 				for(temp = 0; temp < trans_i; temp++)
 				{
-					cout << "." ;
+					//cout << "." ;
 					if(trans[temp].id == input_idx2)
 					{
-						cout << "trans for in2 found" << endl;
+						//cout << "existed trans found! " << endl;
 						gates[gate_i].in2 = &trans[temp];
 						trans[temp].to = &gates[gate_i];
+						gates[gate_i].in2 -> id = input_idx2;
+						input_idx2 = 0;
 						break;
 					}
 				}
-
-				cout << temp << endl;
+				
+				//cout << "temp: " << temp << endl;
+				//cout << "trans_i: " << trans_i << endl;
+				//cout << "input_idx2: " << input_idx2 << endl;
 					
-				if(-1 != input_idx2)
+				if(input_idx2 != 0)
 				{
-					cout << "|" ;
+					//cout << "using new trans" << endl;
 					gates[gate_i].in2 = &trans[trans_i];
-					cout << "|" ;
 					trans[trans_i].to = &gates[gate_i];
-					cout << "|" ;
+					gates[gate_i].in2 -> id = input_idx2;
+
+					input_idx2 = 0;
 					trans_i++;
 				}
+
 				
-				gates[gate_i].in2 -> id = input_idx2;
-				
-				cout << endl << ">>> middle line as input2 of this gate(on mem):" << trans[trans_i].to ->type << endl ;
-				cout << "middle line as in2 of this gate:" << gates[gate_i].in2 -> id << endl ;
+				//cout << endl << ">>> middle line as input2 of this gate(on mem):" << trans[trans_i - 1].to ->type << endl ;
+				//cout << "middle line as in2 of this gate:" << gates[gate_i].in2 -> id << endl ;
 			}
 
-			cout << "in2 process complete //" << endl << endl;
+			//cout << "in2 process complete //" << endl << endl;
 		}
 		else
 		{
@@ -248,7 +265,7 @@ int fileReading()
 		
 
 		// ---------------------- out -----------------------
-		cout << "output_idx = " << output_idx << endl;
+		//cout << "output_idx = " << output_idx << endl;
 		for(int i = 0; i < output_num; i++)
 		{
 
@@ -256,7 +273,7 @@ int fileReading()
 			{
 				gates[gate_i].out = &output[i];
 				output[i].from = &gates[gate_i];
-				cout << "output found" << endl;
+				//cout << "output found" << endl;
 				output_idx = 0;
 				break;
 			}
@@ -265,38 +282,43 @@ int fileReading()
 
 		if(output_idx != 0) //new, using middle line
 		{
-			cout << "tans_i : " << trans_i << " / output_idx : " << output_idx << endl;
+			//cout << "tans_i : " << trans_i << " / output_idx : " << output_idx << endl;
 
 			for(int i = 0; i < trans_i; i++)
 			{
+				//cout << ".";
 				if(trans[i].id == output_idx)
-				{
+				{	
+					//cout << "existed trans found! " << endl;
 					gates[gate_i].out = &trans[i];
 					trans[i].from = &gates[gate_i];
+					gates[gate_i].out -> id = output_idx;
+					output_idx = 0;
 					break;
 				}
 			}
 				
-			if (gates[gate_i].out == 0)
+			if (output_idx != 0)
 			{
+				//cout << "using new trans" << endl;
 				gates[gate_i].out = &trans[trans_i];
 				trans[trans_i].from = &gates[gate_i];
 				trans[trans_i].id = output_idx;
+				gates[gate_i].out -> id = output_idx;
 				trans_i++;
 			}
 
-			gates[gate_i].out -> id = output_idx;
 
-			cout << endl << ">>> middle line as output of this gate(on mem):" << gates[gate_i].out->id <<endl ;
+			//cout << endl << ">>> middle line as output of this gate(on mem):" << gates[gate_i].out->id <<endl ;
 		
 		}
 
-		cout << "Checking for gate para correctness : " << gates[gate_i].out->id << endl ;
+		//cout << "Checking for gate para correctness : " << gates[gate_i].out->id << endl ;
 
 		gate_i++;
 
 
-		cout << "//-------------gate recognize end----------------" << endl << endl;
+		//cout << "//-------------gate recognize end----------------" << endl << endl;
 
 	}
 
